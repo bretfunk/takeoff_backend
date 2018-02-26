7fdd91fcc7365c6d11f4dc41d836b5343438b0d1
BFunk <bret.e.funk@gmail.com>
2018-02-26 15:47:28 -0500

    fixed route typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get '/test' => 'api/v1/users#test'
-  get '/api/v1/users/:address' => 'api/v1/users#show'
+  get '/api/v1/user/:address' => 'api/v1/users#show'
   post '/api/v1/users/:address' => 'api/v1/users#new'
 end
